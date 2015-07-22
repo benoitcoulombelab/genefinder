885be974cc65baf8b4a63130432c362de63ab0a1
Christian Poitras <christian.poitras@ircm.qc.ca>
2015-07-22 15:50:50 -0400

    Fixed javadoc.

--- src/main/java/ca/qc/ircm/genefinder/util/ExceptionUtils.java
+++ src/main/java/ca/qc/ircm/genefinder/util/ExceptionUtils.java
@@ -7,7 +7,7 @@ public class ExceptionUtils {
     /**
      * Returns exception if it's already a {@link RuntimeException}. If exception is not a {@link RuntimeException},
      * exception is packaged into a new {@link PackagedRuntimeException}.
-     * 
+     *
      * @param e
      *            exception to package, if necessary
      * @param message
@@ -16,45 +16,53 @@ public class ExceptionUtils {
      *         cause
      */
     public static RuntimeException optionallyPackageRuntimeException(Throwable e, String message) {
-        if (e instanceof RuntimeException) {
-            return (RuntimeException) e;
-        } else {
-            return new PackagedRuntimeException(message, e);
-        }
+	if (e instanceof RuntimeException) {
+	    return (RuntimeException) e;
+	} else {
+	    return new PackagedRuntimeException(message, e);
+	}
     }
 
     /**
      * Throws exception if it's assignable to clazz. Otherwise, this method does nothing.
-     * 
+     *
+     * @param <T>
+     *            exception type
      * @param exception
      *            exception
      * @param clazz
      *            expected exception's clazz
+     * @throws T
+     *             exception
      */
     public static <T extends Throwable> void throwExceptionIfMatch(Throwable exception, Class<? extends T> clazz)
-            throws T {
-        if (exception != null && clazz.isAssignableFrom(exception.getClass())) {
-            @SuppressWarnings("unchecked")
-            T exceptionCast = (T) exception;
-            throw exceptionCast;
-        }
+	    throws T {
+	if (exception != null && clazz.isAssignableFrom(exception.getClass())) {
+	    @SuppressWarnings("unchecked")
+	    T exceptionCast = (T) exception;
+	    throw exceptionCast;
+	}
     }
 
     /**
      * Throws an {@link InterruptedException} if current thread is interrupted.
-     * 
+     *
      * @param message
      *            exception's message
+     * @throws InterruptedException
+     *             if current thread is interrupted
      */
     public static void throwIfInterrupted(String message) throws InterruptedException {
-        if (Thread.currentThread().isInterrupted()) {
-            throw new InterruptedException(message);
-        }
+	if (Thread.currentThread().isInterrupted()) {
+	    throw new InterruptedException(message);
+	}
     }
 
     /**
      * Returns first found expection's cause in expection's causes that matches expected class.
-     * 
+     *
+     * @param <T>
+     *            exception type
      * @param e
      *            exception
      * @param clazz
@@ -62,18 +70,18 @@ public class ExceptionUtils {
      * @return expection's cause if it matches expected class
      */
     public static <T extends Throwable> T getCause(Throwable e, Class<T> clazz) {
-        if (e != null) {
-            Throwable cause = e.getCause();
-            while (cause != null) {
-                if (clazz.isAssignableFrom(cause.getClass())) {
-                    @SuppressWarnings("unchecked")
-                    T ret = (T) cause;
-                    return ret;
-                } else {
-                    cause = cause.getCause();
-                }
-            }
-        }
-        return null;
+	if (e != null) {
+	    Throwable cause = e.getCause();
+	    while (cause != null) {
+		if (clazz.isAssignableFrom(cause.getClass())) {
+		    @SuppressWarnings("unchecked")
+		    T ret = (T) cause;
+		    return ret;
+		} else {
+		    cause = cause.getCause();
+		}
+	    }
+	}
+	return null;
     }
 }
