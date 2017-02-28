c111cc878d586432119c79b25b10df01e46c5acb
Christian Poitras <christian.poitras@ircm.qc.ca>
2017-02-28 09:12:06 -0500

    Adds unit test for multiple requests on UniProt.
    Former-commit-id: 9e1ee35ba63835f442bd0f38619a2e75378aba7b

--- src/test/java/ca/qc/ircm/genefinder/annotation/UniprotDownloadProteinMappingServiceTest.java
+++ src/test/java/ca/qc/ircm/genefinder/annotation/UniprotDownloadProteinMappingServiceTest.java
@@ -8,7 +8,9 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.anyDouble;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.anyVararg;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -24,6 +26,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -34,10 +38,12 @@ import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.zip.GZIPOutputStream;
 
 import javax.ws.rs.client.Client;
@@ -72,6 +78,8 @@ public class UniprotDownloadProteinMappingServiceTest {
   private ProgressBar progressBar;
   @Mock
   private FTPClient ftpClient;
+  @Captor
+  private ArgumentCaptor<String> stringCaptor;
   @Rule
   public TemporaryFolder temporaryFolder = new TemporaryFolder();
   private Locale locale = Locale.getDefault();
@@ -171,6 +179,10 @@ public class UniprotDownloadProteinMappingServiceTest {
     verify(target).queryParam("to", "ACC");
     verify(target).queryParam("format", "tab");
     verify(target).queryParam("columns", "id,database(GeneID)");
+    verify(target).queryParam(eq("query"), stringCaptor.capture());
+    List<String> queries = Arrays.asList(stringCaptor.getValue().split(" "));
+    assertEquals(3, queries.size());
+    assertTrue(queries.containsAll(proteinIds));
     verify(target).request();
     verify(request).get(InputStream.class);
     verify(ftpService).anonymousConnect(ncbiConfiguration.ftp());
@@ -232,6 +244,10 @@ public class UniprotDownloadProteinMappingServiceTest {
     verify(target).queryParam("to", "ACC");
     verify(target).queryParam("format", "tab");
     verify(target).queryParam("columns", "id,sequence");
+    verify(target).queryParam(eq("query"), stringCaptor.capture());
+    List<String> queries = Arrays.asList(stringCaptor.getValue().split(" "));
+    assertEquals(3, queries.size());
+    assertTrue(queries.containsAll(proteinIds));
     verify(target).request();
     verify(request).get(InputStream.class);
     verify(proteinService).weight(parseSequence(remoteMappingsPath, proteinIds.get(0)));
@@ -323,6 +339,74 @@ public class UniprotDownloadProteinMappingServiceTest {
         assertEquals(sequenceWeight3, mapping.getMolecularWeight(), 0.001);
       }
       assertNull(mapping.getTaxonomyId());
+    }
+  }
+
+  @Test
+  public void downloadProteinMappings_OverMaxIdsPerRequest() throws Throwable {
+    when(uniprotConfiguration.maxIdsPerRequest()).thenReturn(2);
+    when(parameters.getProteinDatabase()).thenReturn(ProteinDatabase.UNIPROT);
+    when(parameters.isGeneId()).thenReturn(true);
+    when(parameters.isGeneName()).thenReturn(true);
+    when(parameters.isGeneSynonyms()).thenReturn(true);
+    when(parameters.isGeneSummary()).thenReturn(true);
+    final List<String> proteinIds = Files.readAllLines(
+        Paths.get(getClass().getResource("/annotation/uniprot-accessions3.txt").toURI()));
+    byte[] remoteMappings = Files
+        .readAllBytes(Paths.get(getClass().getResource("/annotation/idmapping3-gene").toURI()));
+    when(request.get(InputStream.class)).thenReturn(new ByteArrayInputStream(remoteMappings));
+    Path localGeneInfo = download.resolve("refseq.gene_info.gz");
+    gzip(Paths.get(getClass().getResource("/annotation/refseq.gene_info").toURI()), localGeneInfo);
+    when(ftpService.localFile(geneInfo)).thenReturn(localGeneInfo);
+
+    final List<ProteinMapping> mappings = uniprotDownloadProteinMappingService
+        .downloadProteinMappings(proteinIds, parameters, progressBar, locale);
+
+    verify(restClientFactory).createClient();
+    verify(client).target(mapping);
+    verify(target).queryParam("from", "ACC,ID");
+    verify(target).queryParam("to", "ACC");
+    verify(target).queryParam("format", "tab");
+    verify(target).queryParam("columns", "id,database(GeneID)");
+    verify(target, times(2)).queryParam(eq("query"), stringCaptor.capture());
+    List<String> queries = stringCaptor.getAllValues().stream()
+        .flatMap(q -> Arrays.asList(q.split(" ")).stream()).collect(Collectors.toList());
+    assertEquals(3, queries.size());
+    assertTrue(queries.containsAll(proteinIds));
+    assertEquals(2, stringCaptor.getAllValues().get(0).split(" ").length);
+    assertEquals(1, stringCaptor.getAllValues().get(1).split(" ").length);
+    verify(target, times(2)).request();
+    verify(request, times(2)).get(InputStream.class);
+    verify(ftpService).anonymousConnect(ncbiConfiguration.ftp());
+    verify(ftpService).localFile(geneInfo);
+    verify(ftpService).downloadFile(ftpClient, geneInfo, localGeneInfo, progressBar, locale);
+    assertEquals(3, mappings.size());
+    for (ProteinMapping mapping : mappings) {
+      if (mapping.getProteinId().equals("A0A075B759")) {
+        assertNotNull(mapping.getGenes());
+        assertEquals(2, mapping.getGenes().size());
+        GeneInfo gene = mapping.getGenes().get(0);
+        assertEquals(1L, gene.getId());
+        assertEquals("A1BG", gene.getSymbol());
+        assertEquals("alpha-1-B glycoprotein", gene.getDescription());
+        assertArrayEquals("A1B|ABG|GAB|HYST2477".split("\\|"), gene.getSynonyms().toArray());
+        gene = mapping.getGenes().get(1);
+        assertEquals(2149L, gene.getId());
+        assertEquals("F2R", gene.getSymbol());
+        assertEquals("coagulation factor II thrombin receptor", gene.getDescription());
+        assertArrayEquals("CF2R|HTR|PAR-1|PAR1|TR".split("\\|"), gene.getSynonyms().toArray());
+      } else {
+        assertNotNull(mapping.getGenes());
+        assertEquals(1, mapping.getGenes().size());
+        GeneInfo gene = mapping.getGenes().get(0);
+        assertEquals(4404L, gene.getId());
+        assertEquals("MRX39", gene.getSymbol());
+        assertEquals("mental retardation, X-linked 39", gene.getDescription());
+        assertEquals(null, gene.getSynonyms());
+      }
+      assertNull(mapping.getTaxonomyId());
+      assertNull(mapping.getSequence());
+      assertNull(mapping.getMolecularWeight());
     }
   }
 }
